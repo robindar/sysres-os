05ec53c6878b7fc39cc1a2da6816f91262b07518
David ROBIN <david.robin97@gmail.com>
2018-03-20 12:26:45 +0100

    Add Qemu/Hardware switch for uart functions

--- src/libc/uart/uart.c
+++ src/libc/uart/uart.c
@@ -1,6 +1,9 @@
 #include "uart.h"
 #include "../misc.h"
 
+#define GPIO_BASE       0x3F200000
+
+#ifdef HARDWARE
 /*
  * The following code was originally written
  * by David Welch, and published unlicensed
@@ -13,24 +16,24 @@ extern void PUT32 ( unsigned int, unsigned int );
 extern unsigned int GET32 ( unsigned int );
 extern void dummy ( unsigned int );
 
-#define GPFSEL1         0x3F200004
-#define GPSET0          0x3F20001C
-#define GPCLR0          0x3F200028
-#define GPPUD           0x3F200094
-#define GPPUDCLK0	    0x3F200098
+#define GPFSEL1         (GPIO_BASE + 0x00000004)
+#define GPSET0          (GPIO_BASE + 0x0000001C)
+#define GPCLR0          (GPIO_BASE + 0x00000028)
+#define GPPUD           (GPIO_BASE + 0x00000094)
+#define GPPUDCLK0       (GPIO_BASE + 0x00000098)
 
-#define AUX_ENABLES     0x3F215004
-#define AUX_MU_IO_REG   0x3F215040
-#define AUX_MU_IER_REG  0x3F215044
-#define AUX_MU_IIR_REG  0x3F215048
-#define AUX_MU_LCR_REG  0x3F21504C
-#define AUX_MU_MCR_REG  0x3F215050
-#define AUX_MU_LSR_REG  0x3F215054
-#define AUX_MU_MSR_REG  0x3F215058
-#define AUX_MU_SCRATCH  0x3F21505C
-#define AUX_MU_CNTL_REG 0x3F215060
-#define AUX_MU_STAT_REG 0x3F215064
-#define AUX_MU_BAUD_REG 0x3F215068
+#define AUX_ENABLES     (GPIO_BASE + 0x00015004)
+#define AUX_MU_IO_REG   (GPIO_BASE + 0x00015040)
+#define AUX_MU_IER_REG  (GPIO_BASE + 0x00015044)
+#define AUX_MU_IIR_REG  (GPIO_BASE + 0x00015048)
+#define AUX_MU_LCR_REG  (GPIO_BASE + 0x0001504C)
+#define AUX_MU_MCR_REG  (GPIO_BASE + 0x00015050)
+#define AUX_MU_LSR_REG  (GPIO_BASE + 0x00015054)
+#define AUX_MU_MSR_REG  (GPIO_BASE + 0x00015058)
+#define AUX_MU_SCRATCH  (GPIO_BASE + 0x0001505C)
+#define AUX_MU_CNTL_REG (GPIO_BASE + 0x00015060)
+#define AUX_MU_STAT_REG (GPIO_BASE + 0x00015064)
+#define AUX_MU_BAUD_REG (GPIO_BASE + 0x00015068)
 
 //GPIO14  TXD0 and TXD1
 //GPIO15  RXD0 and RXD1
@@ -94,6 +97,112 @@ unsigned char uart_getc()
 {
 	return uart_recv();
 }
+
+
+#else
+
+enum
+{
+    // The offsets for reach register.
+
+    // Controls actuation of pull up/down to ALL GPIO pins.
+    GPPUD = (GPIO_BASE + 0x94),
+
+    // Controls actuation of pull up/down for specific GPIO pin.
+    GPPUDCLK0 = (GPIO_BASE + 0x98),
+
+    // The base address for UART.
+    UART0_BASE = (GPIO_BASE + 0x00001000),
+
+    // The offsets for reach register for the UART.
+    UART0_DR     = (UART0_BASE + 0x00),
+    UART0_RSRECR = (UART0_BASE + 0x04),
+    UART0_FR     = (UART0_BASE + 0x18),
+    UART0_ILPR   = (UART0_BASE + 0x20),
+    UART0_IBRD   = (UART0_BASE + 0x24),
+    UART0_FBRD   = (UART0_BASE + 0x28),
+    UART0_LCRH   = (UART0_BASE + 0x2C),
+    UART0_CR     = (UART0_BASE + 0x30),
+    UART0_IFLS   = (UART0_BASE + 0x34),
+    UART0_IMSC   = (UART0_BASE + 0x38),
+    UART0_RIS    = (UART0_BASE + 0x3C),
+    UART0_MIS    = (UART0_BASE + 0x40),
+    UART0_ICR    = (UART0_BASE + 0x44),
+    UART0_DMACR  = (UART0_BASE + 0x48),
+    UART0_ITCR   = (UART0_BASE + 0x80),
+    UART0_ITIP   = (UART0_BASE + 0x84),
+    UART0_ITOP   = (UART0_BASE + 0x88),
+    UART0_TDR    = (UART0_BASE + 0x8C),
+};
+
+// Memory-Mapped I/O output
+static inline void mmio_write(uint64_t reg, uint64_t data)
+{
+	*(volatile uint64_t *) reg = data;
+}
+
+// Memory-Mapped I/O input
+static inline uint64_t mmio_read(uint64_t reg)
+{
+	return *(volatile uint64_t *)reg;
+}
+
+void uart_init()
+{
+	// Disable UART0.
+	mmio_write(UART0_CR, 0x00000000);
+	// Setup the GPIO pin 14 && 15.
+
+	// Disable pull up/down for all GPIO pins & delay for 150 cycles.
+	mmio_write(GPPUD, 0x00000000);
+	delay(150);
+
+	// Disable pull up/down for pin 14,15 & delay for 150 cycles.
+	mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+	delay(150);
+
+	// Write 0 to GPPUDCLK0 to make it take effect.
+	mmio_write(GPPUDCLK0, 0x00000000);
+
+	// Clear pending interrupts.
+	mmio_write(UART0_ICR, 0x7FF);
+
+	// Set integer & fractional part of baud rate.
+	// Divider = UART_CLOCK/(16 * Baud)
+	// Fraction part register = (Fractional part * 64) + 0.5
+	// UART_CLOCK = 3000000; Baud = 115200.
+
+	// Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
+	mmio_write(UART0_IBRD, 1);
+	// Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
+	mmio_write(UART0_FBRD, 40);
+
+	// Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
+	mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+
+	// Mask all interrupts.
+	mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
+			(1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
+
+	// Enable UART0, receive & transfer part of UART.
+	mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+}
+
+void uart_putc(unsigned char c)
+{
+	// Wait for UART to become ready to transmit.
+	while ( mmio_read(UART0_FR) & (1 << 5) ) { }
+	mmio_write(UART0_DR, c);
+}
+
+unsigned char uart_getc()
+{
+	// Wait for UART to have received something.
+	while ( mmio_read(UART0_FR) & (1 << 4) ) { }
+	return mmio_read(UART0_DR);
+}
+
+#endif
 
 int uart_puts(const char* str)
 {
