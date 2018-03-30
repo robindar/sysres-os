196aefe297872cc8070c777ceb8119a5d8ba6d7f
David ROBIN <david.robin97@gmail.com>
2018-03-30 17:17:15 +0200

    Add MMU initialization at boot

--- src/boot.s
+++ src/boot.s
@@ -61,17 +61,26 @@ _start:
 	mov sp, #0x8000
 
 	// Enable MMU
-	//msr TTBR0_EL1, X0 // Set TTBR0
-	//msr TTBR1_EL1, X1 // Set TTBR1
-	//msr TCR_El1, X2   // Set TCR
+	ldr X3, =__TTBR0_EL1_start
+	msr TTBR0_EL1, X3 // Set TTBR0
+	ldr X3, =__TTBR1_EL1_start
+	msr TTBR1_EL1, X3 // Set TTBR1
+	// Set TCR
+	mrs X27, TCR_EL1
+	orr X3, X3, #(1 << 38) // TBI1 -> Top byte ignored in address calculation
+	orr X3, X3, #(1 << 37) // TBI0 -> idem
+	orr X3, X3, #(1 << 36) // AS   -> 16 bits of ASID
+	orr X3, X3, #(1 << 31) // TG1  -> 10 : granule size at 4KB
+	msr TCR_El1, X3
 	// Instruction Synchronization Barrier:
 	//  forces the previous changes to be seen
 	//  before enabling the MMU
-	//isb
+	isb
 
-	//mrs X0, SCTLR_EL1 // Read  System Control Register configuration data
-	//orr X0, X0, #1    // Set [M] bit and enable the MMU
-	//msr SCTLR_EL1, X0 // Write System Control Register configuration data
+	//mrs X3, SCTLR_EL1 // Read  System Control Register configuration data
+	//orr X3, X3, #1    // Set [M] bit and enable the MMU
+	//msr SCTLR_EL1, X3 // Write System Control Register configuration data
+	// Instruction Synchronization Barrier:
 	//isb
 
 	mov X0, #0
