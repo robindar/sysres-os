2676fe13a972710ebb82ed2b4ed880486f26d867
David ROBIN <david.robin97@gmail.com>
2018-04-08 18:31:16 +0200

    Fix shift-count overflow warning in MASK(63, )

--- src/memory/mmu.h
+++ src/memory/mmu.h
@@ -8,7 +8,8 @@
 
 #define one_u64 ((uint64_t) 1)
 #define AT(addr) (* (uint64_t *) (addr))
-#define MASK(f,t) ((f < t) ? (one_u64 << (t+1)) - (one_u64 << f)  : (one_u64 << (f+1)) - (one_u64 << t))
+#define MASK(f,t) ((f < t) ? _MASK_(t,f) : _MASK_(f,t))
+#define _MASK_(f,t) ( f == 63 ? 0 : (one_u64 << (f+1)) ) - (one_u64 << t)
 
 #define RAM_SIZE 1073741824 /* 1 Gio */
 #define ID_PAGING_SIZE RAM_SIZE/*2097152 2 Mio */
